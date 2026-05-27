ae60ac92368bc0dc1d284b0688df44d93fe300f3
FernandoHaeser <fernandohaeserr@gmail.com>
2026-05-27 10:29:41 -0300

    fix: adjust version variable

--- Casks/maccopy.rb
+++ Casks/maccopy.rb
@@ -1,8 +1,8 @@
 cask "maccopy" do
-  version "1.1.2"
+  version "untagged-493415f5fa62ac3b3940"
   sha256 "ab0d5ee7de85a7cd03156780a004bddb4015fb68d698853389d42328b6bd1d58"
 
-  url "https://github.com/maccopy/maccopy/releases/download/v#{version}/Maccopy.zip"
+  url "https://github.com/maccopy/maccopy/releases/download/#{version}/Maccopy.zip"
   name "Maccopy"
   desc "Menu bar clipboard history manager"
   homepage "https://maccopy.github.io/maccopy/"
